df910b5337a5583dbe0d1c6deb8b4c790775e7fd
m-m-hajilou <m.m.hajilou@gmail.com>
2020-01-21 10:41:37 +0200

    add funcion selectCellHorizontalMidTDown() and add in selectDosezHomeComputer and test

--- dose.c
+++ dose.c
@@ -169,7 +169,7 @@ int  selectDosezHomeComputer(){
    }
     else if(levelNumberInserted==2)
     {
-      if (selectCellVerticallUpToMid()||selectCellHorizontalLeftToMid()==1 || selectCellHorizontalMidToRight()==1 ||selectCellHorizontalRightToMid()==1 ||  selectCellHorizontalMidToLeft()==1)
+      if (selectCellHorizontalLeftToMid()==1 || selectCellHorizontalMidToRight()==1 ||selectCellHorizontalRightToMid()==1 ||  selectCellHorizontalMidToLeft()==1 || selectCellVerticallUpToMid() || selectCellVerticallMidToDown())
       {
       return 1;
       }
@@ -255,7 +255,19 @@ int selectCellVerticallUpToMid()
   for (int y = 0; y <= 2; y++)
   {
     if(dose[0][y]==2 && dose[1][y]==0 && dose[2][y]==0){
-      dose[1][y]=1;
+      dose[1][y]=2;
+      return 1;
+    }
+  }
+  return 0;
+}
+
+int selectCellVerticallMidToDown()
+{
+  for (int y = 0; y <= 2; y++)
+  {
+    if(dose[0][y]==0 && dose[1][y]==2 && dose[2][y]==0){
+      dose[2][y]=2;
       return 1;
     }
   }
