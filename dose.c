9ce03cd12d4d5ad1dc06fb9d80842de096d9e078
m-m-hajilou <m.m.hajilou@gmail.com>
2020-01-22 10:04:36 +0200

    add funcion selectCellCrosshairsCenterOut ()to selverdose()

--- dose.c
+++ dose.c
@@ -169,7 +169,7 @@ int  selectDosezHomeComputer(){
    }
     else if(levelNumberInserted==2)
     {
-      if (selectCellHorizontalLeftToMid()==1 || selectCellHorizontalMidToRight()==1 ||selectCellHorizontalRightToMid()==1 ||  selectCellHorizontalMidToLeft()==1 || selectCellVerticallUpToMid()==1 || selectCellVerticallMidToDown()==1 || selectCellVerticallDownToMid()==1 || selectCellVerticallMidToUp()==1 || selectCellCrosshairsCenterOut()==1)
+      if (selectCellHorizontalLeftToMid()==1 || selectCellHorizontalMidToRight()==1 ||selectCellHorizontalRightToMid()==1 ||  selectCellHorizontalMidToLeft()==1 || selectCellVerticallUpToMid()==1 || selectCellVerticallMidToDown()==1 || selectCellVerticallDownToMid()==1 || selectCellVerticallMidToUp()==1 || selectCellCrosshairsToCenter()==1 || selectCellCrosshairsCenterOut()==1)
       {
       return 1;
       }
@@ -191,7 +191,13 @@ int selectCellSingel(){
         {
           dose[x][y] = 2;
           checkTrueInsertNumber = 1;
-        }
+        }   }
+    else if(levelNumberInserted==2)
+    {
+      if (selectCellHorizontalLeftToMid()==1 || selectCellHorizontalMidToRight()==1 ||selectCellHorizontalRightToMid()==1 ||  selectCellHorizontalMidToLeft()==1 || selectCellVerticallUpToMid()==1 || selectCellVerticallMidToDown()==1 || selectCellVerticallDownToMid()==1 || selectCellVerticallMidToUp()==1 || selectCellCrosshairsCenterOut()==1 || selectCellCrosshairsToCenter()==1)
+      {
+      return 1;
+      }
       } while(checkTrueInsertNumber == 0);
       return 1;
     }
