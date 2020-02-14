18c80d66ef882d9e29dc877514f34794fcb23029
m-m-hajilou <m.m.hajilou@gmail.com>
2020-02-14 12:51:58 +0330

    clean code

--- dose.c
+++ dose.c
@@ -1,20 +1,25 @@
 #include<stdio.h>//                                        بسم الله  الرحمن الرحیم
 #include<stdlib.h>
 int dose[3][3],turn,levelNumberInserted=1,winer=0;
-void main(int argc, char const *argv[]) {
+void main(int argc, char const *argv[])
+{
 solverdose();
 }
 
-void solverdose(){// سلام چک شود که قبل اینکه عدد وارد شود برسی شود بازی به انتها رسیده است یا خیر.یاعلی علیه السلام
+void solverdose(){
   howstart();
-  while (checkdoseWin()==0 && doseEnd()==0) {//  ویا اگر رسیده است باید به گونه ای تمام شود یاعلی علیه السلام
-    if (turner()==1) {
+  while (checkdoseWin()==0 && doseEnd()==0)
+  {
+    if (turner()==1)
+    {
       displaydose();
       SelectdoseHomeUser();
       turn++;
     }
-     if(checkdoseWin()!=1 && doseEnd()!=1){
-      if (turner()==2) {
+     if(checkdoseWin()!=1 && doseEnd()!=1)
+     {
+      if (turner()==2)
+      {
         controlSelectDoseHomeComputer();
         turn++;
         levelNumberInserted++;
@@ -26,19 +31,24 @@ void solverdose(){// سلام چک شود که قبل اینکه عدد وارد
   printWinerOrMessagefinish();
 }
 
-void howstart(){
+void howstart()
+{
   printf("\t \t \t IN THE NAME OF ALLAH \n Helo gamers \n  welcome to game dose \n  please select how starter game .\n if insertNumber = 1 your starter game or  insertNumber = 2 the computer starter game  *** for inser number in cell must your number true . the number true for select a cell and insert first insert number vertical(4<vertical>0) and then second first insert number horizontal(4<horizontal>0)***  \n \n   Let's start the game \n   \n ");
   displaydose();
   turn=getNumber(1,2);
 }
 
-int getNumber(int low,int hi){
+int getNumber(int low,int hi)
+{
   int inputNumber;
     //printf("%i please insert true number  the number is %s < input number > %s And the home notFull  ",low,hi);//----------------------------------سلام از اینجا درست کردن پیام اینکه  کم و زیاد در متن چاپ شود
-  do{
+  do
+  {
     scanf("%d",&inputNumber);
-    if(inputNumber>=low & inputNumber<=hi){
-    return inputNumber;}
+    if(inputNumber>=low & inputNumber<=hi)
+    {
+    return inputNumber;
+    }
     printf("***error***   your number not true   ***error*** ");
   }  while (inputNumber<=low | inputNumber>=hi);
 }
