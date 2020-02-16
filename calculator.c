a1f1758e3963c9a175b9c815df116597c1d0f50c
m-m-hajilou <m.m.hajilou@gmail.com>
2020-02-16 20:27:04 +0330

    just for test token

---
+++ calculator.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int int main(int argc, char const *argv[])
+{
+  char text [] = "IN THE NAME OF ALLAH";
+  char token=strtok(text," ");
+  printf("%s\n",token);
+  return 0;
+}
+int calculator ()
+{
+
+}
