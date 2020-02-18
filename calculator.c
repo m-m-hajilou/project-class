dcebed14cc8e9edc979118b1dff06aea41993499
m-m-hajilou <m.m.hajilou@gmail.com>
2020-02-18 21:34:24 +0330

    get number and oprator

--- calculator.c
+++ calculator.c
@@ -7,9 +7,13 @@ int main(int argc, char const *argv[])
   return 0;
 }
 
-int calculator()
+float calculator()
 {
-
+  printf("%s\n","IN THE NAME OF ALLH hi this is a calculator first : insert number1  second : insert operator (*,/,+,-) third :insert number2  \n \n");
+  printf("%s\n","please insert first number \n \n");
+  float number1= getNumber();
+  char oprator;
+  scanf("%s\n",oprator); 
+  printf("%s\n","please insert seond number \n \n");
+  float number2= getNumber();
 }
-
-int getNumber()
