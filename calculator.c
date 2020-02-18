caa5983a2820adcaa520c9ada2bf83c3f6267b8c
m-m-hajilou <m.m.hajilou@gmail.com>
2020-02-18 21:12:33 +0330

    clean and add library

--- calculator.c
+++ calculator.c
@@ -1,13 +1,2 @@
-#include<stdio.h>
-#include<string.h>
-int int main(int argc, char const *argv[])
-{
-  char text [] = "IN THE NAME OF ALLAH";
-  char token=strtok(text," ");
-  printf("%s\n",token);
-  return 0;
-}
-int calculator ()
-{
-
-}
+#include <stdio.h>
+##include "calculatorDivisible.h"
