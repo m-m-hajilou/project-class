797b488bc7019fb3b0be8506f0fdb2978d783e46
m-m-hajilou <m.m.hajilou@gmail.com>
2020-02-18 21:24:34 +0330

    add #include "hajilou.h"

--- calculator.c
+++ calculator.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include "hajilou.h"
 #include "calculatorDivisible.h"
 int main(int argc, char const *argv[])
 {
 
   return 0;
 }
+
+int calculator()
+{
+
+}
+
+int getNumber()
