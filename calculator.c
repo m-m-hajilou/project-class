f3b60b27adad517a6b07cb95c4778d6a42f3a905
m-m-hajilou <m.m.hajilou@gmail.com>
2020-02-18 22:00:45 +0330

    change location main to down and add printf for get orator

--- calculator.c
+++ calculator.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include "hajilou.h"
-#include "calculatorDivisible.h"
-int main(int argc, char const *argv[])
-{
-
-  return 0;
-}
-
+//#include "calculatorDivisible.h"
 float calculator()
 {
+  char oprator;
   printf("%s\n","IN THE NAME OF ALLH hi this is a calculator first : insert number1  second : insert operator (*,/,+,-) third :insert number2  \n \n");
   printf("%s\n","please insert first number \n \n");
   float number1= getNumber();
-  char oprator;
-  scanf("%s\n",oprator); 
+
+  printf("%s\n","please insert first number \n \n");
+  scanf("%s\n",oprator);
+
   printf("%s\n","please insert seond number \n \n");
   float number2= getNumber();
+}
+
+int main(int argc, char const *argv[])
+{
+  calculator();
+  return 0;
 }
