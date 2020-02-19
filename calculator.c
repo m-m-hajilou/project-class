45511b014dcf6f382d8052be658f18da243cd2a1
m-m-hajilou <m.m.hajilou@gmail.com>
2020-02-19 19:31:18 +0330

    add javab and print javab for test

--- calculator.c
+++ calculator.c
@@ -3,6 +3,7 @@
 #include "calculatorDivisible.h"
 float calculator()
 {
+  float javab;
   char oprator;
   printf("%s\n","IN THE NAME OF ALLH hi this is a calculator first : insert number1  second : insert operator (*,/,+,-) third :insert number2  \n \n");
   printf("%s\n","please insert first number \n \n");
@@ -16,9 +17,11 @@ float calculator()
 
   switch (oprator) {
     case '*':
-
+    javab=divisible(number1,number2);
+    printf("%i\n",javab );
       break;
   }
+
 }
 
 int main(int argc, char const *argv[])
