9cc7dda7a6f5419ba8353cef0496e8ba2419048b
m-m-hajilou <m.m.hajilou@gmail.com>
2020-02-27 21:45:42 +0330

    ok

--- calculatorDivisible.h
+++ calculatorDivisible.h
@@ -1,5 +1,5 @@
-#ifndef CALCULATORDIVISIBLE_H
-#define CALCULATORDIVISIBLE_H
+#ifndef _CALCULATORDIVISIBLE_H_
+#define _CALCULATORDIVISIBLE_H_
 float divisible (float number1,float number2)
 {
   return(number1/number2);
